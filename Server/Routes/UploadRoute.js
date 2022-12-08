525dea304949ae17078a2d6143ab713e49624f90
chmubarak <chmohamedmubarak@gmail.com>
2022-12-08 13:25:02 +0530

    add image resizing

--- Server/Routes/UploadRoute.js
+++ Server/Routes/UploadRoute.js
@@ -1,21 +1,17 @@
 import express from "express";
 import multer from "multer";
 import authenticate from "../middleware/Auth.js";
+import sharp from "sharp";
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.use(authenticate)
-router.post("/image", upload.single("file"), (req, res) => {
+router.use(authenticate);
+router.post("/image", upload.single("file"), async (req, res) => {
   try {
+    const path=`public/images/${req.body.name}`
+    await sharp(req.file.buffer).resize(600,600).toFile(path)
     return res.status(200).json({ message: "File upload successfully" });
   } catch (err) {
     console.log(err);
