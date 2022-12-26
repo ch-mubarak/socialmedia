a8620157c639de3270c58f65c00aa3c46f7a9cda
chmubarak <chmohamedmubarak@gmail.com>
2022-12-26 20:05:34 +0530

    testing production

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
-import * as dotenv from "dotenv";
-dotenv.config();
 const connectDB = async () => {
   try {
-    console.log(process.env.DATABASE_URL)
     await mongoose.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
