4b00f37f3c07dab2ad7e6bc0a5acf75b04f1f298
chmubarak <chmohamedmubarak@gmail.com>
2022-12-27 10:23:33 +0530

    add new domain to cors

--- Server/config/cors.js
+++ Server/config/cors.js
@@ -1,7 +1,6 @@
 const corsOptions = {
   origin: [
-    "http://localhost:3000",
-    "https://fancy-kashata-ef7b14.netlify.app",
+    "https://hash-tags.netlify.app",
     "http://hashtag.codestreak.in",
     "https://hashtag.codestreak.in",
   ],
